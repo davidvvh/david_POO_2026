47d7a067abe0a3878854fda261b4c19f55c58853
davidvvh <165288004+davidvvh@users.noreply.github.com>
2026-03-17 12:41:40 -0300

    ok

--- 03_bucle_for/static/js/script.js
+++ 03_bucle_for/static/js/script.js
@@ -161,14 +161,14 @@ Recorre el arreglo. Si la temperatura es mayor a 30, inyecta en el párrafo:
 
 function parrafosAdvertencia() {
     let temperaturas = [22, 24, 28, 35, 21, 38];
-    let temperaturasPeligrosas = []
+    let temperaturasPeligrosas = [];
     const container = document.getElementById('resultadoContainer8');
     const result8 = document.getElementById('result8');
     for (let i = 0; i < temperaturas.length; i++) {
-        if (temperaturas[i] >= 30 && temperaturas < 50) {
-            temperaturasPeligrasas.push(temperaturas[i])
-            result8.innerHTML += `<p class="text-danger">¡ALERTA! Temperatura critica de 
-            ${temperaturas[i]} + ${temperaturasPeligrasas} grados. </p><br>`;
+        if (temperaturas[i] >= 30 && temperaturas[i] < 50) {
+            result8.innerHTML +=
+            `<p class="text-danger">¡ALERTA! Temperatura critica de 
+            ${temperaturas[i]} grados.</p><br>`;
         }
     }
     container.classList.remove('d-none');
