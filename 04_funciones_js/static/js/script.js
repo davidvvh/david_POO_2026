26b1cc72907aafe8a135d2cf42a49d8ed74226bb
davidvvh <165288004+davidvvh@users.noreply.github.com>
2026-03-18 15:11:37 -0300

    ok

--- 04_funciones_js/static/js/script.js
+++ 04_funciones_js/static/js/script.js
@@ -1,6 +1,66 @@
 console.log("js conectado...")
 
 /*ejercicio 1*/
-function listarAsistencia() {
-    const input = document.getElementById("input1").value
+
+let asistencia = [];
+function agregarLista(nombre){
+    asistencia.push(nombre);
+    return asistencia.join(", ");
 }
+function registrarAlumno() {
+    const container = document.getElementById("container1");
+    const result = document.getElementById("result1");
+    const input = document.getElementById("input1");
+    let nombre = input.value;
+    let resultado = agregarLista(nombre);
+    result.textContent = resultado;
+    input.value = "";
+    container.classList.remove("d-none");
+}
+
+
+
+/*ejercicio 2*/
+let pacientes = ["carlos", "maria", "diego"]
+function agregarUrgencia(nombre){
+    pacientes.unshift(nombre);
+    return pacientes.join(", ");
+};
+function ingresarUrgencia(){
+    const container = document.getElementById("container2");
+    const result = document.getElementById("result2");
+    const input = document.getElementById("input2");
+    if (input.value !== "") {
+    let nombre = input.value;
+    let resultado = agregarUrgencia(nombre);
+    result.textContent = resultado;
+    input.value = "";
+    container.classList.remove("d-none");
+    } else {
+        alert("el nombre no puede estar vacio")
+    };
+};
+
+
+
+/*ejercicio 3*/
+
+
+
+/*ejercicio 4*/
+
+
+
+/*ejercicio 5*/
+
+
+
+/*ejercicio 6*/
+
+
+
+/*ejercicio 7*/
+
+
+
+/*ejercicio 8*/
